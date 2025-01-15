0b0acd432060a3f3c84f40174fbb6a19f9d63a2f
yesjjin99 <yesjjin99@naver.com>
2025-01-15 16:22:45 +0900

    feat: 상품 요청 시 validation 추가
    상품 이름이 Null 또는 빈 문자열인 경우, 가격이 음수인 경우 validation

--- src/main/java/com/programmers/cafe/dto/ProductRequestDto.java
+++ src/main/java/com/programmers/cafe/dto/ProductRequestDto.java
@@ -1,14 +1,16 @@
 package com.programmers.cafe.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 
 @Getter
 public class ProductRequestDto {
-    @NotNull
+    @NotNull @NotBlank
     private String name;
 
-    @NotNull
+    @NotNull @PositiveOrZero
     private int price;
 
     // TODO: 정적 파일(사진) 등록
