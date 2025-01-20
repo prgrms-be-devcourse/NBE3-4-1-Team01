b5e8387946a163863131047d9e709b8dbe72e6a7
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-01-20 14:04:30 +0900

    fix: test코드 수정

--- src/test/java/com/programmers/cafe/service/OrderServiceTest.java
+++ src/test/java/com/programmers/cafe/service/OrderServiceTest.java
@@ -1,8 +1,13 @@
 package com.programmers.cafe.service;
 
 import com.programmers.cafe.dto.OrderDto;
+import com.programmers.cafe.dto.OrderFilterDto;
 import com.programmers.cafe.entity.Order;
+import com.programmers.cafe.entity.Product;
+import com.programmers.cafe.entity.ProductOrder;
 import com.programmers.cafe.repository.OrderRepository;
+import com.programmers.cafe.repository.ProductOrderRepository;
+import com.programmers.cafe.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -23,23 +29,30 @@ public class OrderServiceTest {
     private OrderService orderService;
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private OrderFilterDto filter;
 
     @BeforeEach
     void initData() {
-        // 테스트 데이터: 12개
+        List<ProductOrder> emptyList = new ArrayList<>();
+        // 테스트 데이터: 16개
         List<Order> orders = List.of(
-                Order.builder().email("example01@example.com").address("대한민국").postalCode("12345").status(0).build(),
-                Order.builder().email("example01@example.com").address("대한민국").postalCode("00000").status(0).build(),
-                Order.builder().email("example02@example.com").address("대한민국").postalCode("00123").status(0).build(),
-                Order.builder().email("example03@example.com").address("대한민국").postalCode("12345").status(1).build(),
-                Order.builder().email("example04@example.com").address("대한민국").postalCode("12345").status(0).build(),
-                Order.builder().email("example04@example.com").address("대한민국").postalCode("00000").status(0).build(),
-                Order.builder().email("example05@example.com").address("대한민국").postalCode("00123").status(0).build(),
-                Order.builder().email("example04@example.com").address("대한민국").postalCode("12345").status(0).build(),
-                Order.builder().email("example04@example.com").address("대한민국").postalCode("00000").status(0).build(),
-                Order.builder().email("example05@example.com").address("대한민국").postalCode("00123").status(0).build(),
-                Order.builder().email("example03@example.com").address("대한민국").postalCode("12345").status(1).build(),
-                Order.builder().email("example04@example.com").address("대한민국").postalCode("56789").status(1).build()
+                Order.builder().email("example01@example.com").address("대한민국").postalCode("12345").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example01@example.com").address("대한민국").postalCode("00000").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example02@example.com").address("대한민국").postalCode("00123").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example03@example.com").address("대한민국").postalCode("12345").status(1).productOrders(emptyList).build(),
+                Order.builder().email("example04@example.com").address("대한민국").postalCode("12345").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example04@example.com").address("대한민국").postalCode("00000").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example05@example.com").address("대한민국").postalCode("00123").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example04@example.com").address("대한민국").postalCode("12345").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example04@example.com").address("대한민국").postalCode("00000").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example05@example.com").address("대한민국").postalCode("00123").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example01@example.com").address("대한민국").postalCode("12345").status(1).productOrders(emptyList).build(),
+                Order.builder().email("example02@example.com").address("대한민국").postalCode("56789").status(1).productOrders(emptyList).build(),
+                Order.builder().email("example05@example.com").address("대한민국").postalCode("00123").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example03@example.com").address("대한민국").postalCode("12345").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example04@example.com").address("대한민국").postalCode("00000").status(0).productOrders(emptyList).build(),
+                Order.builder().email("example05@example.com").address("대한민국").postalCode("00123").status(0).productOrders(emptyList).build()
         );
 
         orderRepository.saveAll(orders);
@@ -55,35 +68,35 @@ public class OrderServiceTest {
 
         // then
         assertThat(orders).isNotNull();
-        assertThat(orders.size()).isEqualTo(12);
+        assertThat(orders.size()).isEqualTo(16);
     }
 
     @Test
     @DisplayName("주문 필터 조회 - 배송 준비중")
     void t2() {
         // given
-        int deliveryStatus = 0;
-        String email = "";
-        int page = 0;
+        filter.setDeliveryStatus(0);
+        filter.setEmail("");
+        int page = 1; // 2페이지
 
         // when
-        List<OrderDto> orders = orderService.getOrderByFilters(deliveryStatus, email, page).getContent();
+        List<OrderDto> orders = orderService.getOrderByFilters(filter, page).getContent();
 
         // then
         assertThat(orders).isNotNull();
-        assertThat(orders.size()).isEqualTo(9);
+        assertThat(orders.size()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("주문 필터 조회 - 배송중")
     void t3() {
         // given
-        int deliveryStatus = 1;
-        String email = "";
+        filter.setDeliveryStatus(1);
+        filter.setEmail("");
         int page = 0;
 
         // when
-        List<OrderDto> orders = orderService.getOrderByFilters(deliveryStatus, email, page).getContent();
+        List<OrderDto> orders = orderService.getOrderByFilters(filter, page).getContent();
 
         // then
         assertThat(orders).isNotNull();
@@ -94,16 +107,16 @@ public class OrderServiceTest {
     @DisplayName("주문 필터 조회 - 이메일")
     void t4() {
         // given
-        int deliveryStatus = 2; // 모두
-        String email = "example01@example.com";
+        filter.setDeliveryStatus(2);
+        filter.setEmail("example01@example.com");
         int page = 0;
 
         // when
-        List<OrderDto> orders = orderService.getOrderByFilters(deliveryStatus, email, page).getContent();
+        List<OrderDto> orders = orderService.getOrderByFilters(filter, page).getContent();
 
         // then
         assertThat(orders).isNotNull();
-        assertThat(orders.size()).isEqualTo(2);
+        assertThat(orders.size()).isEqualTo(3);
     }
 
     @Test
@@ -125,11 +138,11 @@ public class OrderServiceTest {
     void t6() {
         // given
         int page = 0; // 1페이지
-        int deliveryStatus = 0; // 배송 준비중
-        String email = "";
+        filter.setDeliveryStatus(0);
+        filter.setEmail("");
 
         // when
-        List<OrderDto> orders = orderService.getOrderByFilters(deliveryStatus, email, page).getContent();
+        List<OrderDto> orders = orderService.getOrderByFilters(filter, page).getContent();
 
         // then
         assertThat(orders).isNotNull();
@@ -141,11 +154,11 @@ public class OrderServiceTest {
     void t7() {
         // given
         int page = 0; // 1페이지
-        int deliveryStatus = 2;
-        String email = "example01@example.com";
+        filter.setDeliveryStatus(2);
+        filter.setEmail("example01@example.com");
 
         // when
-        List<OrderDto> orders = orderService.getOrderByFilters(deliveryStatus, email, page).getContent();
+        List<OrderDto> orders = orderService.getOrderByFilters(filter, page).getContent();
 
         // then
         assertThat(orders).isNotNull();
